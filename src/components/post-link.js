5cd8b10e083df48d0f268fbae333d309b34ff9ba
Maud Nalpas <maud.nalpas@gmail.com>
2020-04-08 18:44:25 +0200

    feat: consitent height for images + grid responsiveness

--- src/components/post-link.js
+++ src/components/post-link.js
@@ -4,9 +4,13 @@ import { Link } from "gatsby"
 const PostLink = ({ post }) => (
   <article class="card ">
     <Link to={post.frontmatter.path}>
-      {!!post.frontmatter.thumbnail && (
-        <img src={post.frontmatter.thumbnail} alt={post.frontmatter.title + "- Featured Shot"} />
-      )}
+      <div class="card-image-wrapper">
+        <div>
+          {!!post.frontmatter.thumbnail && (
+            <img src={post.frontmatter.thumbnail} alt={post.frontmatter.title + "- Featured Shot"} />
+          )}
+        </div>
+      </div>
     </Link>
     <header>
       <h2 className="post-title">
